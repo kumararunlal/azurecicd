b8b9d390bdc7e6881760869d852da27c2005745e
AKumarm <kumar.arunlal@gmail.com>
2024-11-25 06:02:43 +0000

    firt commit

---
+++ provider.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
+
+  # Update this block with the location of your terraform state file
+  backend "azurerm" {
+    resource_group_name  = "terra-cicd"
+    storage_account_name = "terracicd"
+    container_name       = "tfstate"
+    key                  = "deploy.tfstate"
+    
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "terra_rg" {
+  name     = "terra-rg"
+  location = "West Europe"
+}
